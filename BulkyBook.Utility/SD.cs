8e2fd9d2594a205961d956943f39a305fc5fa49a
PeterTheHackerman <peter.s.bartos@gmail.com>
2020-10-10 17:23:25 +0100

    Cover Type CRUD Using Stored Procedures Finished.

--- BulkyBook.Utility/SD.cs
+++ BulkyBook.Utility/SD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,13 @@ namespace BulkyBook.Utility
 {
     public static class SD
     {
+        public const string Proc_CoverType_Create = "usp_CreateCoverType";
+        public const string Proc_CoverType_Get = "usp_GetCoverType";
+        public const string Proc_CoverType_GetAll = "usp_GetCoverTypes";
+        public const string Proc_CoverType_Update = "usp_UpdateCoverType";
+        public const string Proc_CoverType_Delete = "usp_DeleteCoverType";
+
+
     }
 }
+
